ab617185b1b309887644abc4cb01bf77569d2b59
Richard Kopelow <richardkopelow@gmail.com>
2016-04-18 20:42:32 -0400

    Added functionality to the main menu, and set up the play volume render textures.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+}
