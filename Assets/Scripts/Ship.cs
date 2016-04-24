2e37c119b602a9e48f1ef72ae9196be2872b26ad
Richard Kopelow <richardkopelow@gmail.com>
2016-04-23 22:12:28 -0400

    Increased render texture resolution. Further improved render box. added mouse controls for camera.

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,17 +5,41 @@ public class Ship : MonoBehaviour
 {
     Transform trans;
     Rigidbody rigid;
+
+    Vector3 lastMousePos;
+
     void Start()
     {
         trans = GetComponent<Transform>();
         rigid = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        lastMousePos = Input.mousePosition;
+    }
+    void Update()
+    {
+        #region NonVRCamControl
+
+        trans.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
+        trans.Rotate(Vector3.left, Input.GetAxis("Mouse Y"));
+        trans.localEulerAngles = new Vector3(trans.localEulerAngles.x, trans.localEulerAngles.y, 0);
+        #endregion
     }
     void FixedUpdate()
     {
-        if (rigid.velocity.magnitude<3)
+        Vector3 force = 3 * (trans.forward * Input.GetAxis("Vertical") + trans.right * Input.GetAxis("Horizontal"));
+        if (rigid.velocity.magnitude < 4)
         {
-            Vector3 force = trans.forward * Input.GetAxis("Vertical") + trans.right * Input.GetAxis("Horizontal");
-            rigid.AddForce(force);
+            if (force.x * rigid.velocity.x > 0)
+            {
+                force.x = 0;
+            }
+            if (force.y * rigid.velocity.y > 0)
+            {
+                force.y = 0;
+            }
         }
+        rigid.AddForce(force);
     }
 }
